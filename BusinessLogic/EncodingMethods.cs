c6d0555aab4de816c237bc4f8c60f91ca8660fa9
Jynsaillar <jynsaillar@ymail.com>
2019-06-25 01:03:12 +0200

    Added VS Code launch options; Added project; Added main program logic for re-encoding and saving files recursively;

---
+++ BusinessLogic/EncodingMethods.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Text;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace EncodingMethods
+{
+    public static class DirectoryInfoExtension
+    {
+        public static IEnumerable<FileInfo> GetFilesByExtensions(this DirectoryInfo dirInfo, params string[] extensions)
+        {
+            var allowedExtensions = new HashSet<string>(extensions, StringComparer.OrdinalIgnoreCase);
+
+            return dirInfo.EnumerateFiles()
+                          .Where(f => allowedExtensions.Contains(f.Extension));
+        }
+    }
+
+    public static class DirectoryEncodingConverter
+    {
+        public static System.Text.EncodingProvider EncodingProvider = System.Text.CodePagesEncodingProvider.Instance; // These fixes the missing encodings in .NET Core.
+        public static int ConvertAllFileEncodingsFiltered(string directory, Encoding newEncoding)
+        {
+            if (!Directory.Exists(directory))
+            {
+                Console.WriteLine($"{directory} is not a valid directory or doesn't exist, returning...");
+            }
+
+            var targetDirectoryInfo = new DirectoryInfo(directory);
+
+            int convertedFilesCounter = 0;
+
+            foreach (var file in targetDirectoryInfo.GetFilesByExtensions(".h", ".cpp", ".hpp"))
+            {
+                var fileContent = File.ReadAllText(file.FullName);
+                File.WriteAllText(file.FullName, fileContent, newEncoding);
+                convertedFilesCounter++;
+            }
+
+            return convertedFilesCounter;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using EncodingMethods;
+
+namespace EncodingConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Console.WriteLine("No valid directory paths found. Please provide a valid directory as first argument [0] and the new encoding as second argument [1].");
+            }
+
+            System.Text.Encoding.RegisterProvider(DirectoryEncodingConverter.EncodingProvider); // Registers encoding provider to enable all encodings in .NET Core.
+
+            var targetDirectory = args[0];
+            var newEncodingString = args[1];
+            try
+            {
+                var newEncoding = System.Text.Encoding.GetEncoding(newEncodingString);
+                var convertedFilesCount = DirectoryEncodingConverter.ConvertAllFileEncodingsFiltered(targetDirectory, newEncoding);
+
+                Console.WriteLine($"Converted {convertedFilesCount} files.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Couldn't figure out the provided encoding. Are you sure it is a valid encoding or codepage?\n{e.Message}");
+            }
+
+        }
+    }
+}
